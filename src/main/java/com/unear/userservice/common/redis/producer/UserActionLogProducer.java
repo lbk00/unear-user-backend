b5b52fbfedacb2a49977fe28fbba332c2869439c
lbk00 <lbk6661@gmail.com>
2025-07-28 09:44:55 +0900

    feat : UserActionLogProducer 추가

--- src/main/java/com/unear/userservice/common/redis/producer/UserActionLogProducer.java
+++ src/main/java/com/unear/userservice/common/redis/producer/UserActionLogProducer.java
@@ -1,31 +1,34 @@
 package com.unear.userservice.common.redis.producer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserActionLogProducer {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    // RedisStream 키
     private static final String STREAM_KEY = "stream:user_action_logs";
 
-    // 사용자 행동로그를 RedisStream에 기록
-    public void sendLog(String userId, String actionType, String metadata) {
+    @Async
+    public void sendLog(String userId, String actionType, String screen , String metadata) {
         Map<String, String> data = new HashMap<>();
-        data.put("userId", userId); // 사용자 id
-        data.put("actionType", actionType); // 행동 유형
-        data.put("metadata", metadata); // 부가 정보
-        data.put("timestamp", String.valueOf(System.currentTimeMillis())); // 시간
+        data.put("userId", userId);
+        data.put("actionType", actionType);
+        data.put("screen", screen);
+        data.put("metadata", metadata);
+        data.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        log.info("[로그 전송] userId={}, actionType={}, screen={}, metadata={}", userId, actionType, screen, metadata);
 
-        // XADD
         redisTemplate.opsForStream()
                 .add(MapRecord.create(STREAM_KEY, data));
     }
