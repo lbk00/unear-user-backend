591de36acd10adabf9e1c51150203f94e07bd9da
lbk00 <lbk6661@gmail.com>
2025-07-29 11:11:23 +0900

    feat : OAuth2FailureHandler 로그인 실패시 로그인화면으로 리다이렉트

--- src/main/java/com/unear/userservice/auth/handler/OAuth2FailureHandler.java
+++ src/main/java/com/unear/userservice/auth/handler/OAuth2FailureHandler.java
@@ -3,6 +3,7 @@ package com.unear.userservice.auth.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,15 @@ import java.io.IOException;
 @Component
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
+    private static final String REDIRECT_URL = "https://www.unear.site/login";
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
         log.error("OAuth2 로그인 실패: {}", exception.getMessage(), exception);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write("{ \"error\": \"OAuth2 login failed.\" }");
+
+        response.sendRedirect(REDIRECT_URL);
     }
 }
+
