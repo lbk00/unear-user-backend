af2bb693d6931774171404950c9dfdb4ae52379b
1seyoung <akdlrnal0705@gmail.com>
2025-08-05 17:08:15 +0900

    feat : SSE CORS origin 추가

--- src/main/java/com/unear/userservice/notification/controller/NotificationController.java
+++ src/main/java/com/unear/userservice/notification/controller/NotificationController.java
@@ -3,9 +3,11 @@ package com.unear.userservice.notification.controller;
 import com.unear.userservice.notification.service.impl.RedisSseEmitterPool;
 import com.unear.userservice.notification.service.impl.SseEmitterPool;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -16,6 +18,7 @@ public class NotificationController {
 
     private final RedisSseEmitterPool emitterPool;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping("/subscribe/{userId}")
     public SseEmitter subscribe(@PathVariable Long userId) {
         return emitterPool.connect(userId);
