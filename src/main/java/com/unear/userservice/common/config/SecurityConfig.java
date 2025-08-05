8ab1f0e434f4075516f9402d4f8e2f8b0a4a5e76
1seyoung <akdlrnal0705@gmail.com>
2025-08-05 20:51:31 +0900

    fix : sse 구독 로직 수정

--- src/main/java/com/unear/userservice/common/config/SecurityConfig.java
+++ src/main/java/com/unear/userservice/common/config/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
-            "/auth/oauth/**"
+            "/auth/oauth/**",
+            "/notifications/subscribe/**"
     };
 
     @Bean
--- src/main/java/com/unear/userservice/notification/controller/NotificationController.java
+++ src/main/java/com/unear/userservice/notification/controller/NotificationController.java
@@ -1,12 +1,15 @@
 package com.unear.userservice.notification.controller;
 
+import com.unear.userservice.common.jwt.JwtTokenProvider;
 import com.unear.userservice.notification.service.impl.RedisSseEmitterPool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -16,10 +19,20 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class NotificationController {
 
     private final RedisSseEmitterPool emitterPool;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.OPTIONS})
+
     @GetMapping("/subscribe/{userId}")
-    public SseEmitter subscribe(@PathVariable Long userId) {
+    public SseEmitter subscribe(@PathVariable Long userId , @RequestParam String token) {
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new RuntimeException("Invalid token");
+        }
+
+        Long tokenUserId = jwtTokenProvider.extractUserId(token);
+        if (!userId.equals(tokenUserId)) {
+            throw new RuntimeException("User ID mismatch");
+        }
+
         return emitterPool.connect(userId);
     }
 }
