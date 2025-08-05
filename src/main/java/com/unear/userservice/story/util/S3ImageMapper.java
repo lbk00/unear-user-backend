0846098e4c10b8e5c27646260e219b77f3991193
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-08-06 02:14:59 +0900

    refactor : 쓰이지 않는 import 제거

--- src/main/java/com/unear/userservice/story/util/S3ImageMapper.java
+++ src/main/java/com/unear/userservice/story/util/S3ImageMapper.java
@@ -10,15 +10,12 @@ import java.util.Random;
 @Component
 public class S3ImageMapper {
 
-    private static final String STORY_BASE_URL = "https://your-bucket.s3.ap-northeast-2.amazonaws.com/story";
-    private static final String LOGO_BASE_URL = "https://your-bucket.s3.ap-northeast-2.amazonaws.com/logo";
-
     private static final Map<PlaceCategory, Integer> categoryImageCountMap = new EnumMap<>(PlaceCategory.class);
     private static final Random random = new Random();
 
     static {
-        categoryImageCountMap.put(PlaceCategory.FOOD, 5);     // FOOD1.png ~ FOOD3.png
-        categoryImageCountMap.put(PlaceCategory.CAFE, 2);     // CAFE1.png ~ CAFE2.png
+        categoryImageCountMap.put(PlaceCategory.FOOD, 5);    
+        categoryImageCountMap.put(PlaceCategory.CAFE, 2);    
         categoryImageCountMap.put(PlaceCategory.LIFE, 1);
         categoryImageCountMap.put(PlaceCategory.BEAUTY, 1);
         categoryImageCountMap.put(PlaceCategory.ACTIVITY, 5);
@@ -30,7 +27,7 @@ public class S3ImageMapper {
 
     public static String getRandomBackgroundImageFileName(String categoryCode) {
         try {
-            PlaceCategory category = PlaceCategory.fromCode(categoryCode); // enum 매핑
+            PlaceCategory category = PlaceCategory.fromCode(categoryCode); 
             int maxIndex = categoryImageCountMap.getOrDefault(category, 1);
             int randomIndex = random.nextInt(maxIndex) + 1;
             return String.format("%s%d.png", category.getCode(), randomIndex);
