a31d6cbb068dd34644c376588b84e85c1db0abfb
1seyoung <akdlrnal0705@gmail.com>
2025-08-05 22:32:12 +0900

    chore : 추적용 로깅

--- src/main/java/com/unear/userservice/notification/controller/NotificationController.java
+++ src/main/java/com/unear/userservice/notification/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.unear.userservice.notification.controller;
 import com.unear.userservice.common.jwt.JwtTokenProvider;
 import com.unear.userservice.notification.service.impl.RedisSseEmitterPool;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
+@Slf4j
 @CrossOrigin(origins = {"http://localhost:4000", "https://dev.unear.site"}, allowCredentials = "true")
 public class NotificationController {
 
@@ -25,6 +27,8 @@ public class NotificationController {
 
     @GetMapping("/subscribe/{userId}")
     public SseEmitter subscribe(@PathVariable Long userId , @RequestParam String token) {
+        log.info("🔥 SSE 연결 요청 시작 - userId: {}", userId);
+
         if (!jwtTokenProvider.validateToken(token)) {
             throw new RuntimeException("Invalid token");
         }
