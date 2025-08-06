a45198ebb148beab063e389d2aa98fcb1ffadc67
1seyoung <akdlrnal0705@gmail.com>
2025-08-06 14:38:51 +0900

    chore : SseNotificationService.java 로깅 추가

--- src/main/java/com/unear/userservice/notification/service/impl/SseNotificationService.java
+++ src/main/java/com/unear/userservice/notification/service/impl/SseNotificationService.java
@@ -6,10 +6,12 @@ import com.unear.userservice.notification.dto.response.StampAddedResponse;
 import com.unear.userservice.notification.dto.response.StampCompletedResponse;
 import com.unear.userservice.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SseNotificationService implements NotificationService {
 
     private final RedisSseEmitterPool emitterPool;
@@ -20,28 +22,36 @@ public class SseNotificationService implements NotificationService {
         Object responseDto;
         String eventName;
 
+        log.info("[NOTIFY] Received request: userId={}, type={}", userId, request.getType());
+
         try {
             switch (request.getType()) {
                 case STAMP_ADDED -> {
                     eventName = "stamp-added";
                     responseDto = StampAddedResponse.from(request);
+                    log.debug("[NOTIFY] Type=STAMP_ADDED, eventName={}, response={}", eventName, responseDto);
                 }
                 case STAMP_COMPLETED -> {
                     eventName = "stamp-completed";
                     responseDto = StampCompletedResponse.from(request);
+                    log.debug("[NOTIFY] Type=STAMP_COMPLETED, eventName={}, response={}", eventName, responseDto);
                 }
                 case PAYMENT_SUCCESS -> {
                     eventName = "payment-success";
                     responseDto = PaymentSuccessResponse.from(request);
+                    log.debug("[NOTIFY] Type=PAYMENT_SUCCESS, eventName={}, response={}", eventName, responseDto);
                 }
                 default -> {
+                    log.warn("[NOTIFY] Unknown notification type received: {}", request.getType());
                     return;
                 }
             }
 
             emitterPool.sendToUser(userId, eventName, responseDto);
+            log.info("[NOTIFY] Sent event '{}' to userId={}", eventName, userId);
 
         } catch (Exception e) {
+            log.error("[NOTIFY-FAIL] Failed to send notification to userId={}, err={}", userId, e.toString());
             emitterPool.remove(userId);
         }
     }
