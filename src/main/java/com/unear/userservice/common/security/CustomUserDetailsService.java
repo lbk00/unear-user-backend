dade6cb65babb8476bc8228e034ef1189a69d7fd
lbk00 <lbk6661@gmail.com>
2025-08-22 12:49:56 +0900

    feat : transactional 추가

--- src/main/java/com/unear/userservice/common/security/CustomUserDetailsService.java
+++ src/main/java/com/unear/userservice/common/security/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +16,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
         return new CustomUser(user);
     }
 
+    @Transactional(readOnly = true)
     public CustomUser loadUserByUserId(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다: " + userId));
